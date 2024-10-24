97d55a7184092d5594e00305a474ee111e90d33e
burmist-git <burm.leonid@gmail.com>
2024-10-24 13:50:32 +0200

    Update

--- ctapipe.sh
+++ ctapipe.sh
@@ -6,9 +6,10 @@ out_log_file="ctapipe.log"
 
 function printHelp {
     echo " --> ERROR in input arguments "
-    echo " [0] -d  : build apptainer (singularity) no modules need to be loaded"
-    echo " [0] -t  : test sif file"
-    echo " [0] -h  : print help"
+    echo " [0] -d         : build apptainer (singularity) no modules need to be loaded"
+    echo " [0] -t         : test sif file"
+    echo " [0] --testlive : live test in cscs"
+    echo " [0] -h         : print help"
 }
 
 if [ $# -eq 0 ] 
@@ -42,6 +43,33 @@ else
 	echo "singularity run $out_sif_file ctapipe-process --help-all"
 	#
 	singularity run $out_sif_file ls /DBscan_on_simtel_data/
+    elif [ "$1" = "--testlive" ]; then
+	sif_file_in_cscs="/scratch/snx3000/lburmist/singularity/23.10.2024/ctapipe.sif"
+        simtelIn="/scratch/snx3000/lburmist/simtel_data/proton_st/data/corsika_run1000.simtel.gz"
+	singularity run $sif_file_in_cscs which ctapipe-info
+	singularity run $sif_file_in_cscs ctapipe-info --version
+	singularity run -B /scratch/snx3000/lburmist/:/scratch/snx3000/lburmist/ $sif_file_in_cscs python3 DBscan_on_simtel_data_stereo.py --astropytable $simtelIn
+
+	#
+	dataOIdirPreff="../scratch/simtel_data/gamma_st/"
+	dataOI_npe_dirPreff="../scratch/simtel_data/gamma_st/npe/"
+	#
+	#dataOIdirPreff="../scratch/simtel_data/proton_st/"
+	#dataOI_npe_dirPreff="../scratch/simtel_data/proton_st/npe/"
+	#
+	mkdir -p $dataOI_npe_dirPreff
+	simtelIn=$dataOIdirPreff"/data/corsika_run1.simtel.gz"
+	outpkl=$dataOI_npe_dirPreff"/corsika_run1.npe.pkl"
+	outcsv=$dataOI_npe_dirPreff"/corsika_run1.npe.csv"
+	#
+	pixel_mapping_csv="pixel_mapping.csv"
+        isolated_flower_seed_super_flower_csv="isolated_flower_seed_super_flower.list"
+        isolated_flower_seed_flower_csv="isolated_flower_seed_flower.list"
+	all_seed_flower_csv="all_seed_flower.list"
+	python DBscan_on_simtel_data_stereo.py --trg $simtelIn $outpkl $outcsv $pixel_mapping_csv $isolated_flower_seed_super_flower_csv $isolated_flower_seed_flower_csv $all_seed_flower_csv
+	
+
+
     elif [ "$1" = "-h" ]; then
         printHelp
     else
